7ebe3a45521b9fa0b63519f3f8fedbde4d787121
chiefBlack <chiefmolapo@gmail.com>
2023-06-30 22:01:12 +0200

    sendMail

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000; // Change the port number if needed
+const port = 8080; // Change the port number if needed
 
 // Define a route
 app.get("/", (req, res) => {
