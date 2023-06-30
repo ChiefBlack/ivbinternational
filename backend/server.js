08edbe91f688172b0577b993885169f28da72188
chiefBlack <chiefmolapo@gmail.com>
2023-06-30 20:54:05 +0200

    sending email and a form

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000; // Change the port number if needed
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Send a response to the client
+app.get("/", (req, res) => {
+  res.send("Hello, World!"); // Send a response to the client
 });
 
 // Apply online route
-app.post('/apply-online', (req, res) => {
+app.post("/apply-online", (req, res) => {
   // Extract name, surname, and email from request body
   const { name, surname, email } = req.body;
 
   // Send email to admin
-  sendEmailToAdmin(email);
+  sendEmailToAdmin(email, name, surname);
 
-  res.send('Application received'); // Send a response to the client
+  res.send("Application received"); // Send a response to the client
 });
 
 // Function to send email to admin
-const sendEmailToAdmin = (email) => {
+const sendEmailToAdmin = (email, name, surname) => {
   // Nodemailer configuration
-  const nodemailer = require('nodemailer');
+  const nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
     // Configure your email provider here
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-email-password',
+      user: "your-email@gmail.com",
+      pass: "your-email-password",
     },
   });
 
   // Email options
   const mailOptions = {
-    from: 'your-email@gmail.com',
-    to: 'admin@123.com',
-    subject: 'New application received',
+    from: "your-email@gmail.com",
+    to: "admin@123.com",
+    subject: `New application received
+    ${(name, surname)}`,
     text: `New application received from: ${email}`,
   };
 
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
     } else {
-      console.log('Email sent:', info.response);
+      console.log("Email sent:", info.response);
     }
   });
 };
-
 
 // Start the server
 app.listen(port, () => {
